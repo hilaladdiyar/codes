c42700845485f8a7fcfc4a8e994da62206e6c97b
Hilaladdiyar Muhammad Nur <hilaladdiyar@gmail.com>
2021-06-22 16:39:56 +0700

    Create breadcrumb-list-updated.php

---
+++ breadcrumb-list-updated.php
@@ -0,0 +1,18 @@
+/**
+ * Filter the output of Yoast breadcrumbs to remove <span class="breadcrumb_last"> tags in the last breadcrumb item added by the plugin
+ * @param $output
+ *
+ * @return mixed
+ */
+function j_filter_yoast_breadcrumb_single_link_output( $link, $breadcrumb ){
+
+	  if ( strpos( $link, 'breadcrumb_last' ) !== false ) {
+        $link = str_replace( '</li>', '', $link );
+        $link = str_replace( 'span', 'li', $link );
+    }else{
+        $link .= '</li>';
+    }
+
+    return $link;
+}
+add_filter( 'wpseo_breadcrumb_single_link', 'j_filter_yoast_breadcrumb_single_link_output' );
